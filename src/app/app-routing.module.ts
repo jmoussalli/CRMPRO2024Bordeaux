ffdf5658ea679dd7df52d9cef5e42be4dc7b557a
cyran <gs.gouin@gmail.com>
2024-02-19 15:02:28 +0100

    add client folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageListClientsComponent} from "./clients/pages/page-list-clients/page-list-clients.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: PageListClientsComponent},
+  {path:'a', component: PageListClientsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
